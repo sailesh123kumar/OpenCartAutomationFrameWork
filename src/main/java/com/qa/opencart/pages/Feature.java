267b5c2955f3be2795cfc8da59df00e80c33d246
sailesh123kumar <saileshkumar17893@gmail.com>
2024-07-15 10:41:38 +0530

    feature 3 Updated and ready to release

--- src/main/java/com/qa/opencart/pages/Feature.java
+++ src/main/java/com/qa/opencart/pages/Feature.java
@@ -6,7 +6,8 @@ public class Feature {
 	public static void main(String[] args) {
 		
 		System.out.println("Feature 1 Completed");
-		System.out.println("Feature 1 Completed");
+		System.out.println("Feature 2 Completed");
+		System.out.println("Feature 3 Completed");
 		
 	}
 
