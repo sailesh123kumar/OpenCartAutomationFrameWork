fa2a3268281dbc45a8c23c885632730ec1aad11b
sailesh123kumar <saileshkumar17893@gmail.com>
2024-07-15 10:39:05 +0530

    feature to completed for tagging v2

---
+++ src/main/java/com/qa/opencart/pages/Feature.java
@@ -0,0 +1,13 @@
+package com.qa.opencart.pages;
+
+public class Feature {
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Feature 1 Completed");
+		System.out.println("Feature 1 Completed");
+		
+	}
+
+}
